cb5f5727e456eec0d58321dc690a7e752dd2f99a
Torkel Aannestad <torkel.aannestad@outlook.com>
2025-01-05 12:55:13 +0100

    useActiveNavItem Hook

---
+++ hooks/useActiveNavItem.ts
@@ -0,0 +1,11 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+
+export function useActiveNavItem(href: string) {
+    const pathname = usePathname()
+    if (href === "/"){
+      return pathname === href
+    } 
+    return pathname.startsWith(href)
+}
