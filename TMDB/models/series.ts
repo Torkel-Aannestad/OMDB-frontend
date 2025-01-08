c1603ed7427445cc800fa616b7a114cf456fd811
Torkel Aannestad <torkel.aannestad@outlook.com>
2025-01-08 09:40:04 +0100

    api models: series

---
+++ TMDB/models/series.ts
@@ -0,0 +1,114 @@
+import {
+  Genre,
+  ProductionCompany,
+  ProductionCountry,
+  SpokenLanguage,
+} from "./shared";
+
+export type Serie = {
+  id: number;
+  name: string;
+  original_name: string;
+  overview: string;
+  genre_ids: Genre[];
+  poster_path: string;
+  backdrop_path: string;
+  media_type: string;
+  first_air_date: string;
+  original_language: string;
+  origin_country: string[];
+  popularity: number;
+  vote_average: number;
+  vote_count: number;
+  adult: boolean;
+};
+
+export type SerieDetails = {
+  adult: false;
+  backdrop_path: "/gc8PfyTqzqltKPW3X0cIVUGmagz.jpg";
+  created_by: CreatedBy[];
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: LastEpisodeToAir;
+  name: string;
+  next_episode_to_air: NextEpisodeToAir;
+  networks: Network[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  seasons: Season[];
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+};
+
+type CreatedBy = {
+  id: number;
+  credit_id: string;
+  name: string;
+  original_name: string;
+  gender: number;
+  profile_path: string;
+};
+
+type LastEpisodeToAir = {
+  air_date: string;
+  episode_number: number;
+  id: number;
+  name: string;
+  overview: string;
+  production_code: string;
+  season_number: number;
+  still_path: string;
+  vote_average: number;
+  vote_count: number;
+};
+
+type NextEpisodeToAir = {
+  id: number;
+  name: string;
+  overview: string;
+  vote_average: number;
+  vote_count: number;
+  air_date: string;
+  episode_number: number;
+  production_code: string;
+  runtime: number;
+  season_number: number;
+  show_id: number;
+  still_path: string;
+};
+
+type Network = {
+  name: string;
+  id: number;
+  logo_path: string;
+  origin_country: string;
+};
+
+type Season = {
+  air_date: string;
+  episode_count: number;
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string;
+  season_number: number;
+  vote_average: number;
+};
