8a3c108ee068c247b20a8d3ea06291f7ea309000
Torkel Aannestad <torkel.aannestad@outlook.com>
2025-01-08 10:41:49 +0100

    api models: seasons

---
+++ TMDB/models/seasons.ts
@@ -0,0 +1,42 @@
+import { Crew } from "./credits";
+
+export type SeasonDetails = {
+  id: number;
+  air_date: string;
+  episodes: Episode[];
+  name: string;
+  overview: string;
+  poster_path: string;
+  season_number: number;
+};
+
+type Episode = {
+  id: number;
+  name: string;
+  overview: string;
+  air_date: string;
+  episode_number: number;
+  crew: Crew[];
+  guest_stars: GuestStar[];
+  production_code: string;
+  season_number: number;
+  still_path: string;
+  vote_average: number;
+  vote_count: number;
+  runtime: number;
+  show_id: number;
+};
+
+type GuestStar = {
+  id: number;
+  credit_id: string;
+  order: number;
+  character: string;
+  adult: boolean;
+  gender: number | null;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string | null;
+};
