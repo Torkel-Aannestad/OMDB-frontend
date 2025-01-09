05da811f7604c777b4ac86f04626735ea485cb4f
Torkel Aannestad <torkel.aannestad@outlook.com>
2025-01-09 11:41:25 +0100

    tmdb models: people

---
+++ tmdb/models/people.ts
@@ -0,0 +1,31 @@
+import { Movie } from "./movies";
+import { Serie } from "./series";
+
+export type Person = {
+  id: number;
+  name: string;
+  original_name: string;
+  profile_path: string;
+  known_for_department: string;
+  gender: number;
+  popularity: number;
+  known_for: Array<Movie | Serie>;
+  adult: boolean;
+};
+
+export type PersonDetails = {
+  id: number;
+  name: string;
+  also_known_as: string[];
+  biography: string;
+  birthday: string;
+  deathday?: string;
+  gender: number;
+  homepage?: string;
+  imdb_id: string;
+  known_for_department: string;
+  place_of_birth: string;
+  popularity: number;
+  profile_path: string;
+  adult: boolean;
+};
