5a6803785fe85b1360e0f837da54d8d9fa437b6a
Torkel Aannestad <torkel.aannestad@outlook.com>
2025-01-30 08:32:36 +0100

    Replaced prop type for container

--- components/container.tsx
+++ components/container.tsx
@@ -1,19 +1,14 @@
 import { cn } from "@/utils/tailwind";
+import { ComponentProps } from "react";
 
-interface ContainerProps {
-  children: React.ReactNode;
-  className?: string;
-}
-
-export function Container({ children, className }: ContainerProps) {
+export function Container({ className, ...props }: ComponentProps<"div">) {
   return (
     <div
       className={cn(
-        "mx-auto w-full px-4 sm:px-6 lg:px-8",
-        className && className
+        "mx-auto w-full px-4 sm:px-6 lg:px-8 overflow-hidden",
+        className
       )}
-    >
-      {children}
-    </div>
+      {...props}
+    />
   );
 }
