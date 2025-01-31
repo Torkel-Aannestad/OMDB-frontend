4815132338e5541009b2175bd0855b770490c3b6
Torkel Aannestad <torkel.aannestad@outlook.com>
2025-01-31 11:02:02 +0100

    header absolute positioning

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -14,12 +14,15 @@ import { useActiveNavItem } from "@/hooks/useActiveNavItem";
 
 export function SiteHeader() {
   return (
-    <header>
+    <header className="absolute top-0 z-40 w-full">
       <Container>
-        <div className="flex flex-col items-center sm:flex-row bg-background lg:gap-4">
+        <div className="flex flex-col items-center sm:flex-row bg-transparent lg:gap-4">
           <Link
             href="/"
-            className="self-start inline-flex cursor-pointer items-center px-1 text-2xl lg:text-3xl font-bold text-neutral-800 py-2 mt-4 sm:my-8 sm:pr-8"
+            className={cn(
+              "self-start inline-flex cursor-pointer items-center px-1 text-2xl lg:text-3xl font-bold text-foreground py-2 mt-4 sm:my-8 sm:pr-8",
+              "sm:hover:text-muted-foreground duration-300"
+            )}
           >
             OMDB
           </Link>
@@ -55,16 +58,16 @@ function NavItem({ title, href, icon, className }: NavItem) {
     <Link
       href={href}
       className={cn(
-        "inline-flex flex-col sm:flex-row cursor-pointer items-center px-1 sm:text-lg  font-semibold text-neutral-800 my-4 sm:my-8",
+        "group inline-flex flex-col sm:flex-row cursor-pointer items-center px-1 sm:text-lg  font-semibold text-foreground my-4 sm:my-8",
         className && className
       )}
     >
-      <Icon className="h-6 sm:h-4 lg:h-6 lg:mr-2" />{" "}
+      <Icon className="h-6 sm:h-4 lg:h-6 lg:mr-2 group-hover:stroke-muted-foreground transition duration-300" />{" "}
       <span
         className={cn(
           isActive
-            ? "border-neutral-800 sm:border-b"
-            : "sm:border-b sm:border-transparent sm:hover:border-neutral-600 hover:text-neutral-600  "
+            ? "border-foreground sm:border-b"
+            : "sm:border-b sm:border-transparent sm:group-hover:border-muted-foreground group-hover:text-muted-foreground duration-300 "
         )}
       >
         {title}
