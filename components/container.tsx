bdd3db7a6847997d8be4bd2458326fa1c9c1a577
Torkel Aannestad <torkel.aannestad@outlook.com>
2025-02-09 10:51:17 +0100

    ContainerWithSpacing component

--- components/container.tsx
+++ components/container.tsx
@@ -12,3 +12,18 @@ export function Container({ className, ...props }: ComponentProps<"div">) {
     />
   );
 }
+
+export function ContainerWithSpacing({
+  className,
+  ...props
+}: ComponentProps<"div">) {
+  return (
+    <div
+      className={cn(
+        "container px-4 md:px-10 lg:px-20 xl:px-28 2xl:px-32 mt-4 md:mt-8 xl:mt-12 space-y-4 md:space-y-8 xl:space-y-12",
+        className
+      )}
+      {...props}
+    />
+  );
+}
