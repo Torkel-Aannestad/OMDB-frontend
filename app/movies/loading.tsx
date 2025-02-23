ea15e91de2295e35c4e27add69b7c5e4630a43dc
Torkel Aannestad <torkel.aannestad@outlook.com>
2025-02-23 14:56:27 +0100

    loading fix

--- app/movies/loading.tsx
+++ app/movies/loading.tsx
@@ -3,19 +3,21 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <Container className="space-y-8">
+    <div className="container space-y-8">
       <Skeleton className="h-[60vh] min-h-[560px] rounded-md" />
       <Skeleton className="h-8 w-64" />
-      <div className="flex flex-wrap">
-        {Array.from({ length: 20 }).map((_, i) => (
-          <div
-            key={i}
-            className="basis-1/2 rounded-md pb-4 pr-4 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
-          >
-            <Skeleton className="aspect-poster w-full" />
-          </div>
-        ))}
-      </div>
-    </Container>
+      <Container>
+        <div className="flex flex-wrap">
+          {Array.from({ length: 20 }).map((_, i) => (
+            <div
+              key={i}
+              className="basis-1/2 rounded-md pb-4 pr-4 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
+            >
+              <Skeleton className="aspect-poster w-full" />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
   );
 }
--- app/search/loading.tsx
+++ app/search/loading.tsx
@@ -4,7 +4,6 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function Loading() {
   return (
     <div className="container space-y-8">
-      <Skeleton className="h-[60vh] min-h-[560px] rounded-md" />
       <Skeleton className="h-8 w-64" />
       <Container>
         <div className="flex flex-wrap">
